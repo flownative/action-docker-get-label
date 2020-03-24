708169c420a90ecd0807562eed807699c183398c
Robert Lemke <robert@flownative.com>
2020-03-24 07:47:17 +0100

    Support password protected registries

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -x
 
+if [ "${INPUT_REGISTRY_PASSWORD}" != "" ]; then
+    echo "${INPUT_REGISTRY_PASSWORD}" | docker login -u "${INPUT_REGISTRY_USERNAME}" --password-stdin "${INPUT_REGISTRY_ENDPOINT}"
+fi
+
 docker pull "${INPUT_IMAGE}"
 echo "::set-output name=value::$(docker inspect "${INPUT_IMAGE}" | jq -r ".[0].Config.Labels[\"${INPUT_LABEL}\"]")"
