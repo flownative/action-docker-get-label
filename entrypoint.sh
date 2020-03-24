228cb6d514a512e9852e9757e4ee17195780fd16
Robert Lemke <robert@flownative.com>
2020-03-24 07:51:24 +0100

    Display some debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -x
 
+echo "Pulling image ${INPUT_IMAGE} from ${INPUT_REGISTRY_ENDPOINT} authenticating with user ${INPUT_REGISTRY_USERNAME} ..."
+
 if [ "${INPUT_REGISTRY_PASSWORD}" != "" ]; then
     echo "${INPUT_REGISTRY_PASSWORD}" | docker login -u "${INPUT_REGISTRY_USERNAME}" --password-stdin "${INPUT_REGISTRY_ENDPOINT}"
 fi
